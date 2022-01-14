9075c3a50343c5e63e384e63e951b189812a261a
ReidWilliamson <reidwilliamson2@gmail.com>
2022-01-14 15:08:35 -0500

    create contact component and route

--- react-app-114/src/App.js
+++ react-app-114/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { About } from ".components/About";
 
 function App() {
   return (
@@ -13,12 +14,22 @@ function App() {
               <Nav.Link as={Link} to="/">
                 Home
               </Nav.Link>
-              <Nav.Link>About</Nav.Link>
-              <Nav.Link href="#pricing">Contact</Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact">
+                Contact
+              </Nav.Link>
             </Nav>
           </Container>
         </Navbar>
       </>
+      <div>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ react-app-114/src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Contact = () => {
+  return (
+    <div>
+      <h3>Oh hey</h3>
+    </div>
+  );
+};
